f9d89bf4186fbfc690203f4e596c3af5a1899368
lnberry <lnberry@zone06-eb.cs.ucalgary.ca>
2016-04-01 14:57:36 -0600

    kinda working it is over writing strangly and we don't know why but it works through 2 iterations

--- A5.c
+++ A5.c
@@ -4,118 +4,169 @@
 #include "queuestruct.c"
 
 
+int interator=19;
+int currThread;
+typedef struct
+{
+    prod_cons_queue* queue;
+    pthread_mutex_t* locker;
+    int* id;
+} producerStruct;
 
 typedef struct
 {
-	prod_cons_queue* queue;
-	int* id;
-}holder;
+    prod_cons_queue* queue;
+    pthread_mutex_t* locker;
+} consumerStruct;
 
 void* messageForProducerThread(void* args)
 {
-	printf("producer thread inializer\n");
-	holder* queueAndId = (holder*) args;
-	prod_cons_queue* maggie=queueAndId->queue;
-	int Identify =*queueAndId->id;
-	if(DEBUG==1)
+    printf("producer thread inializer\n");  
+    // grab queue holder from args
+    producerStruct* queueAndId = (producerStruct*) args;
+    
+    // grab queue
+    prod_cons_queue* maggie = queueAndId->queue;
+
+    // grab locker
+    pthread_mutex_t* lock = queueAndId->locker;
+
+        
+    // grab id
+    int Identify = queueAndId->id;
+    
+    if(DEBUG==0)
 	{
 		printf("variables initalized\n");
-	}
-
-	for (int i=0; i<10; i++)
-	{
-			if(DEBUG==0)
-			{
-				printf("inside for loop (producer thread) iteration: %i\n", i);
-			}
-			queue_add(maggie, Identify);
-	}
-	if (DEBUG==0)
-	{
-		printf("id: %i\n", Identify);
-	}
+	} 
+    for (int i=0; i<10; i++)
+    {
+        if(DEBUG==0 && currThread<3)
+        {
+            printf("inside for loop (producer thread) iteration: %i\n", i);
+        }
+        pthread_mutex_lock(&lock);
+        queue_add(maggie, Identify);
+        pthread_mutex_unlock(&lock);
+		interator--;
+    }
+    /* DEBUG*/
+   for(int i=0;i<20;i++)
+    {
+		if(currThread==3)
+		{
+        	printf("id: %i\t%i: %i\n", Identify, i, maggie->element[i]);
+    	}
+	} 
+    
+    if (DEBUG==0)
+    {
+        printf("id: %i\n", Identify);
+    }
 }
 
 void* consumerInit(void* args)
 {
-	printf("consumer thread inializer\n");
-	prod_cons_queue* q= (prod_cons_queue*) args;
-	int result = queue_remove(q);
-	printf("result is: %i \n", result);
-	//TODO
-	//needs to get the producer threads id somehow.
-	//printf(contents of message);
+    printf("consumer thread inializer\n");
+    
+    // grab consStruct
+    consumerStruct* consStruct = (consumerStruct*) args;
+    
+    // grab queue
+    prod_cons_queue* q = consStruct->queue;
+    
+    // grab locker
+    pthread_mutex_t* lock = consStruct->locker;
+
+    pthread_mutex_lock(&lock);
+    int result = queue_remove(q);
+    pthread_mutex_unlock(&lock);
+    
+    printf("result is: %i \n", result);
 }
 
 
 int main()
 {
-	printf("Program Start\n");
-	prod_cons_queue* queue;
-	holder queueHolder;
+    printf("Program Start\n");
 
-	queue_initialize(queue);
-	queueHolder.queue=queue;
+    // create locker
+    pthread_mutex_t* lock = PTHREAD_MUTEX_INITIALIZER;
 
-	if(DEBUG==1)
+    // initialize threads
+    pthread_t producerThreads[10];
+    pthread_t consumerThread[1];
+    
+    if(DEBUG==1)
 	{
-		printf("remaininelems: %i\n", queue->remaining_elements);
-	}
-
-	if(DEBUG==1)
-	{	
-		printf("left initalize\n");
-	}
-	pthread_t producerThreads[10];
-	pthread_t consumerThread[1];
-	
-	if(DEBUG==0)
-	{	
 		printf("threads created\n");
 	}
+    
+    // initialize the queue
+    prod_cons_queue* queue;
+    queue_initialize(&queue);
 
-	if(DEBUG==1)
-	{		
-		printf("queueHolder created\n");
+    /*if(DEBUG==1)
+	{
+		printf("remaininelems: %i\n", queue->remaining_elements); 
+    }*/
+
+    // initialize the holder for producer
+    producerStruct prodStruct;
+    prodStruct.queue=&queue;
+    prodStruct.locker=&lock;
+    
+    if(DEBUG==1)
+	{
+		printf("prodStruct created\n");
 	}
-	void* result;
-	if(DEBUG==0)
-	{		
+
+    // initialize the holder for consumer
+    consumerStruct consStruct;
+    consStruct.queue=&queue;
+    consStruct.locker=&lock;
+    
+    if(DEBUG==1)
+	{
+		printf("prodStruct created\n");
+	}
+
+    // result placeholder
+    void* result;
+    
+    if(DEBUG==1)
+	{
 		printf("result created\n");
-	}	
-	for (int k=0; k<10; k++)
-	{
-		if(DEBUG==0)
-		{		
-			printf("enter for loop (main)\n");
-		}
-		int* var= k+1;
-		queueHolder.id=&var;
-
-		printf("%i: ", (k+1));
-		pthread_create(&producerThreads[k], NULL, messageForProducerThread, &queueHolder);
-		pthread_join(producerThreads[k], (void**)&result);
-		if(DEBUG==0)
-		{ 
-			printf("end of for loop (main)\n");
-		}
 	}
-	/*for(int k=0; k<10; k++)
-	{
-		if(DEBUG==1)
+    
+    // loop through and create threads
+    for (int k=0; k<10; k++)
+    {
+        if(DEBUG==1)
+        {
+            printf("enter for loop (main): %i\n", k);
+        }
+        
+        // set the id
+        int* id= k+1;
+		currThread=k+1;        
+		prodStruct.id = id;
+        
+        /// create the thread
+        pthread_create(&producerThreads[k], NULL, messageForProducerThread, &prodStruct);
+        /*if(currThread<3)
 		{
-			printf("join for loop(main)\n");
-		}
-		
-		if(DEBUG==1)
-		{
-			printf("end of join for loop (main) iteration %i\n", (k+1));
-		}
-	}*/		
-	printf("\n");
-	pthread_create(&consumerThread[0], NULL, consumerInit, &queue);
-	pthread_join(consumerThread[0], (void**)&result);
+			printf("what went in: %i  what came out: %i\n", id, queue->element[interator]);
+		}*/
+        // join the thread
+        pthread_join(producerThreads[k], (void**)&result);
+    }    
+    printf("\n");
+    
+    // create and join the consumer thread
+    pthread_create(&consumerThread[0], NULL, consumerInit, &consStruct);
+    pthread_join(consumerThread[0], (void**)&result);
 
-	return 0;
+    return 0;
 }
 
--- queuestruct.c
+++ queuestruct.c
@@ -1,68 +1,119 @@
 #include <stdio.h>
-
+#include <pthread.h>
 #include "queuestruct.h"
+
 int DEBUG=1;
+
 void queue_initialize(prod_cons_queue *q)
 {
+	printf("queue inializer\n"); 
+ 
+    // initialize the queue 
+	for (int i=0; i<20; i++)
+	{		
+        if(DEBUG==0)
+        {
+            printf("for loop(queue inialize): %i \n", i);
+        }
 
-
-	printf("queue inializer\n");
-
-	for (int i=0; i<MAX_QUEUE_SIZE; i++)
-	{
-		if(DEBUG==0)
-		{	
-			printf("in for loop(queue inialize iteration: %i \n", (i+1));
-		}
-		q->element[i]=NULL;
+        q->element[i] = NULL;
 	}
+    
 	if(DEBUG==0)
 	{
-		printf("exited for loop");
-	}
+    printf("exited for loop\n");
+    }
+    
+    // initialize head and tail
 	q->head = NULL;
 	q->tail = NULL;
+        
+    if(DEBUG==0)
+	{
+		printf("initialize head and tail\n");
+    }
 
+    // initialize remaining_elements
 	q->remaining_elements=MAX_QUEUE_SIZE;
-
 }
 
 
 
 void queue_add(prod_cons_queue *q, int element)
 {
-	q->head=20;
-	q->tail=(q->remaining_elements)-1;
-	printf("%i \n",q->tail);
-	if((q->tail>=0)&&(q->tail<=20))
+    
+    // make the head position the end of the queue
+	q->head=19;
+    
+    // make the tail position the index of the remaining element
+    q->tail=(q->remaining_elements)-1;
+	if(q->tail>19)
 	{
-		q->element[q->tail]= element;
-		q->remaining_elements=q->tail;
+		q->tail=0;
 	}
+    
+
+    
+    // check if the tail is within the array 
+	if((q->tail>=0)&&(q->tail<=19)&&((q->element[q->tail])<1 || (q->element[q->tail])>10))
+	{
+        // put the id into that position
+		q->element[q->tail]= element;
+        
+        // make the remaining element the tail
+		q->remaining_elements=q->remaining_elements-1;
+		if(q->remaining_elements>=20)
+		{
+			q->remaining_elements=0;
+		}
+ 
+	}
+           if(DEBUG==1)
+    {
+        printf("tail:%i\telem:%i\n", q->tail,q->element[q->tail]);
+    }
 	else
 	{
-		if(DEBUG==1)
-		{
-			printf("have reached the head of the queue (queue_add in queuestruct)\n");
-		}
-	}
+		if(DEBUG==0)
+        {
+            printf("have reached the head of the queue (queue_add in queuestruct)\n");
+        }
+    } 
 }
 
 int queue_remove(prod_cons_queue *q)
 {
-	if(q->element[q->head]!=NULL)
-	{
-		if(DEBUG==1)
+    if(DEBUG==1)
+    {
+        printf("head: %i\ttail: %i\tremain: %i\telem: %i\n", q->head, q->tail, q->remaining_elements, q->element[q->head]);
+    }
+    
+	if((q->element[q->head])!=NULL)
+	{		
+        int value = q->element[q->head];
+        q->element[q->head]=-3;
+        q->head=q->head - 1;
+		q->remaining_elements=q->remaining_elements+1;
+
+		if(q->remaining_elements>20)
 		{
-			printf("there is something in the end of the array\n");
+			q->remaining_elements=20;
 		}
-		int value = q->element[q->head];
-		q->head=q->head - 1;
-		return value;
+	if (q->tail==0)
+	{
+		q->tail=19;
+	}
+	else
+	{
+		q->tail=q->tail-1;
+	}
+
+        return value;
 	}
 	else
 	{
 		printf("there isn't anything in the end of the array\n");
-		return 0;
+		
+        return 0;
 	}
 }
